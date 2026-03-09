1e9b5dad4264844c002b6bdefec5c5547178126a
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-03-09 16:15:44 +0530

    Simplify MongoDB connection logic

--- config/db.js
+++ config/db.js
@@ -17,14 +17,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-  await mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-  });
-  console.log("MongoDB connected");
- } catch (error) {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
     console.error(error);
-     console.log("MongoDB Not  Connected");
+    console.log("MongoDB Not Connected");
     process.exit(1);
   }
 };
