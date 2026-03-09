b28842f6587a8b3ef3f96873ae4628988436753d
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-03-09 18:19:59 +0530

    Log Mongo URI status on server start

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
+    console.log("Mongo URI:", process.env.MONGO_URI ? "Loaded" : "Missing");
     await connectDB();
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
