08deadfb89166ffac0a46dfe1acd149671e71a9c
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-03-09 18:21:16 +0530

    Refactor MongoDB connection error messages
    Updated error handling for MongoDB connection.

--- config/db.js
+++ config/db.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("MongoDB connected");
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
@@ -20,8 +5,9 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
+    console.error("MongoDB connection error:");
+    console.log("MongoDB not connection");
     console.error(error);
-    console.log("MongoDB Not Connected");
     process.exit(1);
   }
 };
