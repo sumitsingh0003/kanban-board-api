13ea00d9c395ea81deeeda18334840163b4bd578
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-03-09 14:45:08 +0530

    Modify CORS settings to allow all origins
    Updated CORS configuration to allow all origins and specified HTTP methods.

--- server.js
+++ server.js
@@ -16,20 +16,26 @@ const app = express();
 
 connectDB();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://kan-ban-board-ai.vercel.app"
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://kan-ban-board-ai.vercel.app"
+// ];
+
+// app.use(cors({
+//   origin: function(origin, callback){
+//     if(!origin || allowedOrigins.includes(origin)){
+//       callback(null, true);
+//     }else{
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true
+// }));
 
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin || allowedOrigins.includes(origin)){
-      callback(null, true);
-    }else{
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: false
 }));
 
 app.use(express.json());
