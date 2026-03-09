0cd47ae80ce029a19f46e69c5ccba0133da0abd0
Sumit singh <61839226+sumitsingh0003@users.noreply.github.com>
2026-03-09 15:58:29 +0530

    Enhance MongoDB connection with new options
    Updated MongoDB connection options to use new URL parser and unified topology.

--- config/db.js
+++ config/db.js
@@ -1,13 +1,34 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (error) {
+  await mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+  });
+
+  console.log("MongoDB connected");
+catch (error) {
     console.error(error);
+ console.log("MongoDB Not  Connected");
     process.exit(1);
   }
+    
 };
 
 module.exports = connectDB;
